67a1b8a584e88f39aab1faeba7a8e90d64d3d563
Matthew Filo <mfilo5145@sdsu.edu>
2021-09-13 19:28:05 -0700

    Initial A2 Files

---
+++ A2/ai-search-NPuzzle-skeleton-code/explored.py
@@ -0,0 +1,37 @@
+"""
+Class for maintaining explored sets
+"""
+
+class Explored(object):
+    """
+    Maintain an explored set.  Assumes that states are hashable
+    (e.g. state is represented by a tuple)
+    """
+
+    def __init__(self):
+        "__init__() - Create an empty explored set"
+        
+        raise NotImplemented
+
+    def exists(self, state):
+        """
+        exists(state) - Has this state already been explored?
+
+        :param state:  Hashable problem state
+        :return: True if already seen, False otherwise4
+        """
+
+        raise NotImplemented
+
+
+    def add(self, state):
+        """
+        add(state) - Add a given state to the explored set
+
+        :param state:  A problem state that is hashable, e.g. a tuple
+        :return: None
+        """
+
+        raise NotImplemented
+
+
